8780b4606f6e5c449a0369aef973e81a104f78d0
Neo Anderson <neo@lyferise.com>
2021-05-24 17:06:39 +0100

    Offer single item.

--- cube-core/src/main/java/com/lyferise/cube/concurrency/RingBuffer.java
+++ cube-core/src/main/java/com/lyferise/cube/concurrency/RingBuffer.java
@@ -1,4 +1,46 @@
 package com.lyferise.cube.concurrency;
 
-public class RingBuffer {
+public class RingBuffer<T> {
+    public static final int DEFAULT_CAPACITY = 16;
+
+    private final int capacity;
+    private final T[] buffer;
+    private int readSequenceNumber;
+    private int writeSequenceNumber = -1;
+
+    public RingBuffer() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public RingBuffer(final int capacity) {
+        this.capacity = capacity;
+        this.buffer = (T[]) new Object[capacity];
+    }
+
+    public T next() {
+        return buffer[readSequenceNumber++ % capacity];
+    }
+
+    public boolean offer(final T value) {
+        if (isFull()) return false;
+        buffer[(writeSequenceNumber++ + 1) % capacity] = value;
+        return true;
+    }
+
+    public int size() {
+        return (writeSequenceNumber - readSequenceNumber) + 1;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isFull() {
+        return size() == capacity;
+    }
+
+    public boolean isEmpty() {
+        return writeSequenceNumber < readSequenceNumber;
+    }
 }
--- cube-core/src/test/java/com/lyferise/cube/concurrency/RingBufferTest.java
+++ cube-core/src/test/java/com/lyferise/cube/concurrency/RingBufferTest.java
@@ -1,4 +1,28 @@
 package com.lyferise.cube.concurrency;
 
+import org.junit.jupiter.api.Test;
+
+import static com.lyferise.cube.concurrency.RingBuffer.DEFAULT_CAPACITY;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public class RingBufferTest {
+
+    @Test
+    public void shouldOfferSingleItem() {
+
+        // empty
+        final RingBuffer<Integer> ringBuffer = new RingBuffer<>();
+        assertThat(ringBuffer.getCapacity(), is(equalTo(DEFAULT_CAPACITY)));
+        assertThat(ringBuffer.size(), is(equalTo(0)));
+        assertThat(ringBuffer.isEmpty(), is(equalTo(true)));
+        assertThat(ringBuffer.isFull(), is(equalTo(false)));
+
+        // single item
+        assertThat(ringBuffer.offer(5), is(equalTo(true)));
+        assertThat(ringBuffer.size(), is(equalTo(1)));
+        assertThat(ringBuffer.isEmpty(), is(equalTo(false)));
+        assertThat(ringBuffer.isFull(), is(equalTo(false)));
+    }
 }
