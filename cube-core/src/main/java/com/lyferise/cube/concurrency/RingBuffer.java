5161e41687e2a9797d3018dd9814c54c8a27efb7
Neo Anderson <neo@lyferise.com>
2021-05-25 20:32:42 +0100

    Implement BlockingQueue.

--- cube-core/src/main/java/com/lyferise/cube/concurrency/RingBuffer.java
+++ cube-core/src/main/java/com/lyferise/cube/concurrency/RingBuffer.java
@@ -1,6 +1,11 @@
 package com.lyferise.cube.concurrency;
 
-public class RingBuffer<T> {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class RingBuffer<T> implements BlockingQueue<T> {
     public static final int DEFAULT_CAPACITY = 16;
 
     private final int capacity;
@@ -18,6 +23,7 @@ public class RingBuffer<T> {
         this.buffer = (T[]) new Object[capacity];
     }
 
+    @Override
     public T poll() {
         if (isEmpty()) return null;
         final T nextValue = buffer[readSequenceNumber % capacity];
@@ -25,6 +31,22 @@ public class RingBuffer<T> {
         return nextValue;
     }
 
+    @Override
+    public T element() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T peek() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(final T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public boolean offer(final T value) {
         if (isFull()) return false;
         final int nextSequenceNumber = writeSequenceNumber + 1;
@@ -33,6 +55,97 @@ public class RingBuffer<T> {
         return true;
     }
 
+    @Override
+    public T remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void put(final T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offer(final T t, final long timeout, final TimeUnit unit) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T take() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T poll(final long timeout, final TimeUnit unit) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int remainingCapacity() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(final Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends T> values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(final Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <V> V[] toArray(final V[] values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int drainTo(final Collection<? super T> values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int drainTo(final Collection<? super T> values, final int maxElements) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public int size() {
         return (writeSequenceNumber - readSequenceNumber) + 1;
     }
@@ -45,6 +158,7 @@ public class RingBuffer<T> {
         return size() == capacity;
     }
 
+    @Override
     public boolean isEmpty() {
         return writeSequenceNumber < readSequenceNumber;
     }
