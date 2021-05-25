04b3ed5a8d258697e6566ed7a31c13de7bf0730a
Neo Anderson <neo@lyferise.com>
2021-05-25 11:54:25 +0100

    Fix stress test.

--- cube-core/src/test/java/com/lyferise/cube/concurrency/RingBufferStressTest.java
+++ cube-core/src/test/java/com/lyferise/cube/concurrency/RingBufferStressTest.java
@@ -1,25 +1,27 @@
 package com.lyferise.cube.concurrency;
 
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.BitSet;
 import java.util.concurrent.CountDownLatch;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public class RingBufferStressTest {
 
-    @Disabled
     @Test
     @SneakyThrows
     public void shouldReadAndWriteUsingMultipleThreads() {
 
+        // 10 million messages
         final int messageCount = 10000000;
         final RingBuffer<Integer> ringBuffer = new RingBuffer<>(1024);
 
+        // writers
         final CountDownLatch writers = new CountDownLatch(1);
-
-        final BitSet all = new BitSet();
         final Thread writer = new Thread(() -> {
             for (int i = 0; i < messageCount; i++) {
                 while (!ringBuffer.offer(i)) {
@@ -29,22 +31,27 @@ public class RingBufferStressTest {
             writers.countDown();
         });
 
+        // readers
+        final BitSet bitSet = new BitSet();
         final Thread reader = new Thread(() -> {
-            while (!ringBuffer.isEmpty() && writers.getCount() > 0) {
+            while (!ringBuffer.isEmpty() || writers.getCount() > 0) {
                 final Integer value = ringBuffer.poll();
                 if (value != null) {
-                    all.set(value);
+                    bitSet.set(value);
                 }
             }
         });
 
+        // start threads
         writer.start();
         reader.start();
 
+        // wait for threads to terminate
         writer.join();
         reader.join();
 
-        System.out.println(all.cardinality());
-        System.out.println(all.size());
+        // verify messages seen by readers
+        assertThat(bitSet.cardinality(), is(equalTo(messageCount)));
+        assertThat(bitSet.length(), is(equalTo(messageCount)));
     }
 }
