ca82aeb2a6ab9351ef42c719a11e0e36f2f3c236
Cheradenine Zakalwe <zakalwe@lyferise.com>
2021-06-01 15:00:01 +0100

    More tests (WiP).

--- cube-lang/src/test/java/com/lyferise/cube/lang/parser/CubeLexerTest.java
+++ cube-lang/src/test/java/com/lyferise/cube/lang/parser/CubeLexerTest.java
@@ -2,6 +2,8 @@ package com.lyferise.cube.lang.parser;
 
 import com.lyferise.cube.lang.elements.Identifier;
 import com.lyferise.cube.lang.elements.Symbol;
+import com.lyferise.cube.lang.elements.constants.IntConstant;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.lyferise.cube.lang.parser.CubeLexer.tokenize;
@@ -61,5 +63,19 @@ public class CubeLexerTest {
                         new Identifier("test1"),
                         new Identifier("_test1"),
                         new Identifier("__test2")));
+    }
+
+    @Test
+    @Disabled
+    public void shouldTokenizeIntConstants() {
+        assertThat(
+                tokenize("0 1 -1 +1 -2147483648 2147483647"),
+                contains(
+                        new IntConstant(0),
+                        new IntConstant(1),
+                        new IntConstant(-1),
+                        new IntConstant(1),
+                        new IntConstant(Integer.MIN_VALUE),
+                        new IntConstant(Integer.MAX_VALUE)));
     }
 }
