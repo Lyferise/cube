395d20ce4d9920bce026140617a6aff6dd68901d
Cheradenine Zakalwe <zakalwe@lyferise.com>
2021-06-02 08:18:18 +0100

    WAL data file.

---
+++ cube-cluster/src/main/java/com/lyferise/cube/node/wal/DataFile.java
@@ -0,0 +1,109 @@
+package com.lyferise.cube.node.wal;
+
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+
+public class DataFile {
+    private final RandomAccessFile file;
+    private long readPosition;
+    private long writePosition;
+    private long entrySequence;
+
+    @SneakyThrows
+    public DataFile(final String path) {
+
+        // file
+        final var f = new File(path);
+        final var newFile = f.createNewFile();
+        file = new RandomAccessFile(f, "rw");
+
+        // header
+        if (newFile) {
+            writePosition = 24;
+            readPosition = 24;
+            file.writeLong(0);
+            file.writeLong(writePosition);
+            file.writeLong(readPosition);
+        } else {
+            entrySequence = file.readLong();
+            writePosition = file.readLong();
+            readPosition = file.readLong();
+        }
+    }
+
+    @SneakyThrows
+    public WalEntry read() {
+
+        // done?
+        if (!canRead()) return null;
+
+        // entry
+        file.seek(readPosition);
+        final var entrySequence = file.readLong();
+        final var checkPosition = file.readLong();
+        if (readPosition != checkPosition) {
+            throw new UnsupportedOperationException("WAL position check failed");
+        }
+        final var length = file.readInt();
+        final var crc = file.readInt();
+
+        // data
+        final var data = new byte[length];
+        var n = 0;
+        while (n < length) {
+            n += file.read(data, n, length - n);
+        }
+
+        // CRC
+        final WalEntry entry = new WalEntry(entrySequence, data);
+        if (entry.getCrc() != crc) {
+            throw new UnsupportedOperationException("WAL CRC check failed");
+        }
+
+        // header
+        readPosition = file.getFilePointer();
+        file.seek(16);
+        file.writeLong(readPosition);
+        return entry;
+    }
+
+    @SneakyThrows
+    public void write(final WalEntry entry) {
+
+        // entry
+        entrySequence = entry.getSequence();
+        final var data = entry.getData();
+
+        // write
+        file.seek(writePosition);
+        file.writeLong(entrySequence);
+        file.writeLong(writePosition);
+        file.skipBytes(4);
+        file.writeInt(data.length);
+        file.writeInt(entry.getCrc());
+        file.write(data);
+        writePosition = file.getFilePointer();
+
+        // header
+        file.seek(0);
+        file.writeLong(entrySequence);
+        file.writeLong(writePosition);
+    }
+
+    @SneakyThrows
+    public void flush() {
+        file.getChannel().force(true);
+    }
+
+    @SneakyThrows
+    public void close() {
+        file.close();
+    }
+
+    // TODO: scope
+    public boolean canRead() {
+        return readPosition < writePosition;
+    }
+}
--- cube-cluster/src/main/java/com/lyferise/cube/node/wal/Wal.java
+++ cube-cluster/src/main/java/com/lyferise/cube/node/wal/Wal.java
@@ -4,41 +4,16 @@ import com.lyferise.cube.concurrency.RingBuffer;
 import com.lyferise.cube.node.configuration.WalConfiguration;
 import lombok.SneakyThrows;
 
-import java.io.File;
-import java.io.RandomAccessFile;
-
 public class Wal {
-    private final RandomAccessFile dataFile;
+    private final DataFile dataFile;
     private final RingBuffer<WalEntry> readQueue;
-    private long readPosition;
-    private long writePosition;
-    private long entrySequence;
 
     @SneakyThrows
     public Wal(final WalConfiguration config) {
-
-        // file
-        final var f = new File(config.getDataFile());
-        final var newFile = f.createNewFile();
-        dataFile = new RandomAccessFile(f, "rw");
-
-        // header
-        if (newFile) {
-            writePosition = 24;
-            readPosition = 24;
-            dataFile.writeLong(0);
-            dataFile.writeLong(writePosition);
-            dataFile.writeLong(readPosition);
-        } else {
-            entrySequence = dataFile.readLong();
-            writePosition = dataFile.readLong();
-            readPosition = dataFile.readLong();
-        }
-
-        // queue
+        dataFile = new DataFile(config.getDataFile());
         readQueue = new RingBuffer<>(config.getReadQueueCapacity());
-        while (canRead()) {
-            if (!readQueue.offer(next())) {
+        while (dataFile.canRead()) {
+            if (!readQueue.offer(dataFile.read())) {
                 throw new UnsupportedOperationException("WAL read queue full.");
             }
         }
@@ -51,77 +26,17 @@ public class Wal {
 
     @SneakyThrows
     public void write(final WalEntry entry) {
-
-        // entry
-        entrySequence = entry.getSequence();
-        final var data = entry.getData();
-
-        // write
-        dataFile.seek(writePosition);
-        dataFile.writeLong(entrySequence);
-        dataFile.writeLong(writePosition);
-        dataFile.skipBytes(4);
-        dataFile.writeInt(data.length);
-        dataFile.writeInt(entry.getCrc());
-        dataFile.write(data);
-        writePosition = dataFile.getFilePointer();
-
-        // header
-        dataFile.seek(0);
-        dataFile.writeLong(entrySequence);
-        dataFile.writeLong(writePosition);
-
-        // queue
+        dataFile.write(entry);
         readQueue.put(entry);
     }
 
     @SneakyThrows
     public void flush() {
-        dataFile.getChannel().force(true);
+        dataFile.flush();
     }
 
     @SneakyThrows
     public void close() {
         dataFile.close();
-    }
-
-    @SneakyThrows
-    private WalEntry next() {
-
-        // done?
-        if (!canRead()) return null;
-
-        // entry
-        dataFile.seek(readPosition);
-        final var entrySequence = dataFile.readLong();
-        final var checkPosition = dataFile.readLong();
-        if (readPosition != checkPosition) {
-            throw new UnsupportedOperationException("WAL position check failed");
-        }
-        final var length = dataFile.readInt();
-        final var crc = dataFile.readInt();
-
-        // data
-        final var data = new byte[length];
-        var n = 0;
-        while (n < length) {
-            n += dataFile.read(data, n, length - n);
-        }
-
-        // CRC
-        final WalEntry entry = new WalEntry(entrySequence, data);
-        if (entry.getCrc() != crc) {
-            throw new UnsupportedOperationException("WAL CRC check failed");
-        }
-
-        // header
-        readPosition = dataFile.getFilePointer();
-        dataFile.seek(16);
-        dataFile.writeLong(readPosition);
-        return entry;
-    }
-
-    private boolean canRead() {
-        return readPosition < writePosition;
     }
 }
