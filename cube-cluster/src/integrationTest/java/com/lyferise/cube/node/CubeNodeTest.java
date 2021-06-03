c9b792db572ff00970cb2591ad67e5469dd60ce9
Cheradenine Zakalwe <zakalwe@lyferise.com>
2021-06-03 18:59:02 +0100

    Renamed.

--- cube-cluster/src/integrationTest/java/com/lyferise/cube/node/CubeNodeTest.java
+++ cube-cluster/src/integrationTest/java/com/lyferise/cube/node/CubeNodeTest.java
@@ -2,25 +2,25 @@ package com.lyferise.cube.node;
 
 import org.junit.jupiter.api.Test;
 
-public class ClusterIntegrationTest {
+public class CubeNodeTest {
 
     @Test
-    public void shouldReconnectClient() {
-        try (final var cluster = new LocalCluster(1)) {
-            final var client = cluster.connectToNode(0, "test", "test");
-            client.close();
-            client.reconnect("test", "test");
-        }
-    }
-
-    @Test
-    public void shouldStartThreeNodes() {
+    public void shouldConnectToThreeCubeNodes() {
         final var nodeCount = 3;
         try (final var cluster = new LocalCluster(nodeCount)) {
             for (var i = 0; i < nodeCount; i++) {
                 final var client = cluster.connectToNode(i, "test", "test");
                 client.close();
             }
+        }
+    }
+
+    @Test
+    public void shouldReconnectToCubeNode() {
+        try (final var cluster = new LocalCluster(1)) {
+            final var client = cluster.connectToNode(0, "test", "test");
+            client.close();
+            client.reconnect("test", "test");
         }
     }
 }
