697612c7156a6f938d1317b745ab27ee703e4ce8
Cheradenine Zakalwe <zakalwe@lyferise.com>
2021-06-03 07:44:34 +0100

    Code clean.

--- cube-core/src/main/java/com/lyferise/cube/concurrency/Signal.java
+++ cube-core/src/main/java/com/lyferise/cube/concurrency/Signal.java
@@ -2,8 +2,6 @@ package com.lyferise.cube.concurrency;
 
 import lombok.SneakyThrows;
 
-import java.util.function.Supplier;
-
 public class Signal {
     private final Object syncRoot = new Object();
 
@@ -18,9 +16,5 @@ public class Signal {
         synchronized (syncRoot) {
             syncRoot.wait(timeoutMillis);
         }
-    }
-
-    public void waitFor(final Supplier<Boolean> condition) {
-
     }
 }
