a577424afac2f5fe897411ef7051cbb140cb4ed2
Cheradenine Zakalwe <zakalwe@lyferise.com>
2021-06-05 14:02:37 +0100

    Start Cube node.

--- cube-cluster/src/main/java/com/lyferise/cube/node/Application.java
+++ cube-cluster/src/main/java/com/lyferise/cube/node/Application.java
@@ -1,8 +1,15 @@
 package com.lyferise.cube.node;
 
+import static com.lyferise.cube.node.configuration.NodeConfiguration.readConfiguration;
+
 public class Application {
 
     public static void main(final String[] args) {
-        System.out.println("Starting Cube node...");
+        try {
+            final var config = readConfiguration("conf/application.yml");
+            new CubeNode(config);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 }
