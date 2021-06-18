29803096fff68dbefa2acfd5efec564aa8ac4d94
real-skaffen-amtiskaw <skaffen@lyferise.com>
2021-06-18 23:16:36 +0200

    start-node: wait for process to start

--- cube-cluster/unix/start-node.sh
+++ cube-cluster/unix/start-node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+STARTUP_TIMEOUT=5 #seconds
+
 # Which java to use
 if [ -z "$JAVA_HOME" ]; then
   JAVA="java"
@@ -19,9 +21,21 @@ fi
 
 EXECUTABLE=$(ls "$SCRIPT_DIR"/bin/*.jar)
 CONSOLE_OUTPUT_FILE="$SCRIPT_DIR/console.log"
-
 nohup "$JAVA" -jar "$EXECUTABLE" > "$CONSOLE_OUTPUT_FILE" 2>&1 < /dev/null &
-PROCESS=$(pgrep -a java | grep "$SCRIPT_DIR" | grep -v grep)
+
+# Wait for process to start
+echo -n "STARTING..."
+ITERATIONS=$((STARTUP_TIMEOUT*10))
+for (( i = 0; i < ITERATIONS; i++ )); do
+    PROCESS=$(pgrep -a java | grep "$SCRIPT_DIR" | grep -v grep)
+    if [ -n "$PROCESS" ]; then
+      echo " [OK]"
+      break
+    else
+      echo -n "."
+      sleep 0.1
+    fi
+done
 
 if [ -z "$PROCESS" ]; then
   echo "...::: DON'T PANIC :::..."
